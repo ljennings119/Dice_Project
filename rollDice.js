fd16e9ad2242b2e22d8f1f6f993b544f011f5390
ljennings119 <laurenjennings119@gmail.com>
2025-09-25 13:58:46 -0500

    Logic1

--- rollDice.js
+++ rollDice.js
@@ -1,8 +1,8 @@
-function rollDice(faces) {
+export function rollDice(faces) {
   if (!Number.isInteger(faces) || faces <= 1) {
     throw new Error("Invalid number of faces.");
   }
   return Math.floor(Math.random() * faces) + 1;
 }
 
-module.exports = rollDice;
+
